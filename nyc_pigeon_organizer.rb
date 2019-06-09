d00a30176183fbd049ab728e65ff20d464f0aedc
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-09 23:14:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |item|
     item.each do |subItem|
-      subItem.each do |
+      subItem.each do |arrItem|
+        if pigeon_list.include?(arrItem)
+          pigeon_list[item] += subItem
     end
   end
 end
